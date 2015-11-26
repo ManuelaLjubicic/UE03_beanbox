8d198ba562e551a19b4f2d64bc159299b8474d95
ManuelaLjubicic <manuela.ljubicic@students.fhv.at>
2015-11-26 17:35:07 +0100

    ...

---
+++ src/filterBeans/LoadImageBean.java
@@ -0,0 +1,16 @@
+package filterBeans;
+
+import java.awt.*;
+import java.io.Serializable;
+
+/**
+ * Created by manue on 26.11.2015.
+ */
+public class LoadImageBean extends Canvas implements Serializable {
+
+    public LoadImageBean(){
+        setSize(60, 40);
+        setBackground(Color.pink);
+
+    }
+}
