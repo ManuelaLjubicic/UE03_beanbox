ab0e56c476ac19d9495a3b0aac75f11a64278480
ManuelaLjubicic <manuela.ljubicic@students.fhv.at>
2015-12-01 12:18:40 +0100

    ...

---
+++ src/filterBeans/AbstractBeanInfoRadius.java
@@ -0,0 +1,29 @@
+package filterBeans;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+/**
+ * Created by manue on 01.12.2015.
+ */
+public class AbstractBeanInfoRadius extends AbstractBeanInfo {
+
+    private Class usedClass;
+
+    public AbstractBeanInfoRadius(Class usedClass) {
+        super(usedClass);
+        this.usedClass = usedClass;
+    }
+
+
+    @Override
+    public PropertyDescriptor[] getPropertyDescriptors(){
+        try {
+            PropertyDescriptor radius = new PropertyDescriptor("radius", usedClass);
+            PropertyDescriptor rv[] = {radius};
+            return rv;
+        } catch (IntrospectionException e) {
+            throw new Error(e.toString());
+        }
+    }
+}
