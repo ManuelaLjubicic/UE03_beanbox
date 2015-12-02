b81e7f8604d326a50c389bf4445364edab0a067d
ManuelaLjubicic <manuela.ljubicic@students.fhv.at>
2015-12-02 22:25:27 +0100

    ...

--- src/main/TestMe.java
+++ src/main/TestMe.java
@@ -10,7 +10,8 @@ public class TestMe {
 
     }
 
-    //TODO Bildüberlagerung
+    //TODO Bildüberlagerung - zwei bilder, solange zweites bild nicht da ist nichts tun, wenn zweites bild da ist process aufrufen
+    //TODO Anzahl Kastanien brechnen + ausschreiben in ein TXT File - käsealgo (2tes semester) u. pixel des bildes auf weiß umändern
     //TODO Code kommentieren
     //TODO Doku schreiben
     //TODO Manual
